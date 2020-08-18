c37614286c5e5b176cc35bf8f993e8a8444bcbce
Ron Enubs <ronenubs@gmail.com>
2020-08-18 13:58:22 +0800

    method example

---
+++ src/methodexample/MethodExample.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package methodexample;
+
+/**
+ *
+ * @author Enubs
+ */
+public class MethodExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Person person1; //declaration
+        person1 = new Person(); //instantiation
+    }
+    
+    
+}
---
+++ src/methodexample/Person.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package methodexample;
+
+/**
+ *
+ * @author Enubs
+ */
+public class Person {
+    private String lastname;
+    private String firstname;
+    private String middlename;
+    private int age;
+    private String address;
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public void setMiddlename(String middlename) {
+        this.middlename = middlename;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+}
